f96ee7ca10a67f5351533b053acd9990e3058f5d
Schjerven <oyvind.jalland.schjerven@akersolutions.com>
2025-11-16 15:56:14 +0100

    Implemented controller functions

--- TestProject/Controllers/UserController.cs
+++ TestProject/Controllers/UserController.cs
@@ -1,30 +1,74 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Work.ApiModels;
+using Work.Database;
+using Work.Implementation;
 
 namespace Work.Controllers
 {
     [ApiController]
-    public class UserController : ControllerBase
+    public class UserController(UserRepository repository, IMapper mapper) : ControllerBase
     {
+        private readonly UserRepository _repository = repository;
+        private readonly IMapper _mapper = mapper;
 
         public IActionResult Get(Guid id)
         {
-            throw new NotImplementedException("TODO");
+            var user = _repository.Read(id);
+            if (user == null)
+            {
+                return NotFound($"User with user id {id} not found.");
+            }
+
+            return Ok(user);
         }
 
         public IActionResult Post(UserModelDto user)
         {
-            throw new NotImplementedException("TODO");
+            try
+            {
+                _repository.Create(_mapper.Map<User>(user));
+                return Ok();
+            }
+            catch (ArgumentException)
+            {
+                return BadRequest($"User with id {user.Id}");
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
         }
         
         public IActionResult Put(UserModelDto user)
         {
-            throw new NotImplementedException("TODO");
+            try
+            {
+                _repository.Update(_mapper.Map<User>(user));
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
         }
 
         public IActionResult Delete(Guid id)
         {
-            throw new NotImplementedException("TODO");
+            try
+            {
+                var user = _repository.Read(id);
+                if (user == null)
+                {
+                    return NotFound($"User with user id {id} not found.");
+                }
+                _repository.Remove(user);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
         }
 
     }
